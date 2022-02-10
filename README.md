75cef3b7f446db3bcca6506e79396d487a237bb8
Prasenjeet Dutta <prasenjeet.dutta@gmail.com>
2022-02-10 01:19:09 +0000

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # node-leveldb-sample
-A sample project with Node and LevelDB
+
+A sample project with Node and LevelDB.
+
+Similar to, but much simpler than [CryptoEndpoint](https://github.com/pdvcs/crypto-endpoint), which operated a producer/consumer using Java and RocksDB.
+
+## Building and Running
+
+```bash
+npm install
+export DBDIR=/tmp/node-leveldb-sample
+if [ -d $DBDIR ]; then rm -rf $DBDIR; fi && mkdir $DBDIR
+node index.js
+```
---
+++ index.js
@@ -0,0 +1,68 @@
+const axios = require("axios")
+const level = require("level")
+const path = require("path")
+
+var dbPath = process.env.DB_PATH || path.join("/tmp/nodelev", "swdb")
+var db = level(dbPath, { keyEncoding: "binary", valueEncoding: "json" })
+
+async function sleep(ms) {
+    return new Promise((resolve) => setTimeout(resolve, ms))
+}
+
+async function movieDetails(n) {
+    let details = ""
+    await axios
+        .get(`https://swapi.dev/api/films/${n}/`)
+        .then(function (resp) {
+            details = resp.data
+            console.log("received JSON for: " + details.title)
+        })
+        .catch(function (err) {
+            console.log(`error: ${err}`)
+        })
+    return details
+}
+
+async function push() {
+    let i
+    for (i = 1; i <= 6; i++) {
+        console.log(`requesting JSON for: movie ${i}`)
+        let details = await movieDetails(i)
+        if (details !== "") {
+            db.put(details.title, details)
+        }
+        await sleep(2000)
+    }
+}
+
+async function pop() {
+    var readStream = db.createReadStream({})
+    readStream
+        .on("data", async function (data) {
+            console.log(`processing and deleting '${data.key}' ...`)
+            let v = data.value
+            console.log(`   ${v.title}, directed by ${v.director}`)
+            console.log(`   released: ${v.release_date}`)
+            db.del(data.key)
+        })
+        .on("error", async function (err) {
+            console.log(`error reading stream: ${err}`)
+        })
+        .on("close", function () {
+            // console.log("stream closed")
+        })
+        .on("end", function () {
+            // console.log("stream ended")
+        })
+}
+
+async function loopAndProcess() {
+    while (true) {
+        await pop()
+        await sleep(5000)
+    }
+}
+
+;(async () => {
+    await Promise.all([push(), loopAndProcess()])
+})()
